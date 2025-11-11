d38641fd23d03d4dbba41b32b9b46a82b36104fc
Jonathan Ramirez Hernandez <jramirez@SISTEMAS.LOCAL>
2025-11-11 11:34:35 -0600

    update

--- resources/views/atadores/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('page-title', 'Atadores')
-
-@section('content')
-<div class="flex justify-center items-center min-h-[80vh]">
-    <h1 class="text-3xl font-bold text-blue-600">Hola Mundo desde Atadores</h1>
-</div>
-@endsection
---
+++ resources/views/modulos/atadores/programaAtadores/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page-title', 'Atadores')
+
+@section('content')
+<div class="flex justify-center items-center min-h-[80vh]">
+    <h1 class="text-3xl font-bold text-blue-600">Hola Mundo desde Atadores</h1>
+</div>
+@endsection
