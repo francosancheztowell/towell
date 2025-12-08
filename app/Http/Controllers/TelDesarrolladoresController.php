2e9f02366bad166812d881d1cf704168dc4f5518
= <=>
2025-12-08 12:13:06 -0600

    limpindo funcion aja del controlador

--- app/Http/Controllers/TelDesarrolladoresController.php
+++ app/Http/Controllers/TelDesarrolladoresController.php
@@ -8,22 +8,11 @@ class TelDesarrolladoresController extends Controller
 {
     public function index(Request $request)
     {
-        // Base query sin duplicados por nombre
-        $operadoresQuery = TelTelaresOperador::selectRaw('MIN(numero_empleado) as numero_empleado, nombreEmpl')
+        $operadores = TelTelaresOperador::selectRaw('MIN(numero_empleado) as numero_empleado, nombreEmpl')
             ->whereNotNull('nombreEmpl')
             ->groupBy('nombreEmpl')
-            ->orderBy('nombreEmpl');
-
-        // Petición AJAX/JSON: lista simple numero_empleado => nombreEmpl
-        if ($request->ajax() || $request->wantsJson()) {
-            $operadores = (clone $operadoresQuery)
-                ->get()
-                ->pluck('nombreEmpl', 'numero_empleado');
-            return response()->json(['operadores' => $operadores]);
-        }
-
-        // Vista normal
-        $operadores = $operadoresQuery->get();
+            ->orderBy('nombreEmpl')
+            ->get();
 
         return view('modulos.desarrolladores.desarrolladores', compact('operadores'));
     }
