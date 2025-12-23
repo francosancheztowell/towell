19adfe0ce22379f6543a757c656d1fa847ff8c99
= <=>
2025-12-23 09:40:18 -0600

    fix: error de eficiencia inicial y final listo(no desplegaba bien el select al cancelar el movimiento)

--- app/Http/Controllers/ModelosController.php
+++ app/Http/Controllers/ModelosController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Modelos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class ModelosController extends Controller
