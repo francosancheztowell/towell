33ff0010313a32b57fccb01b76d7b6150a97e4a6
Jonny Ramirez <jonnyrmzhdz@gmail.com>
2026-07-27 13:41:22 -0600

    Creando carpeta de modulo de mecanicos en modelos

---
+++ app/Models/Mecanicos/MecVerificaMaquinaLineModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models\Mecanicos;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class MecOrdenTrabajoLineModel extends Model
+{
+    protected $connection = 'sqlsrv';
+
+    protected $table = 'MecOrdenTrabajoLine';
+
+    protected $primaryKey = 'Id';
+
+    public $incrementing = true;
+
+    protected $keyType = 'int';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'Folio',
+        'CveOperador',
+        'NomOperador',
+        'Ajusto',
+        'Reparo',
+        'Cambio',
+        'Lubrico',
+        'FaltaRefacc',
+        'HoraInicial',
+        'HoraFinal',
+        'TotalMinutos',
+        'Calificacion',
+        'CveTejedor',
+        'NomTejedor',
+    ];
+
+    protected $casts = [
+        'Id' => 'integer',
+        'Ajusto' => 'boolean',
+        'Reparo' => 'boolean',
+        'Cambio' => 'boolean',
+        'Lubrico' => 'boolean',
+        'FaltaRefacc' => 'boolean',
+        'TotalMinutos' => 'integer',
+        'Calificacion' => 'integer',
+    ];
+
+    public function ordenTrabajo(): BelongsTo
+    {
+        return $this->belongsTo(MecOrdenTrabajoModel::class, 'Folio', 'Folio');
+    }
+}
